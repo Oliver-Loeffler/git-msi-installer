fcf138fac97174360781fe550c5c84830ecd8993
Oliver-Loeffler <Oliver-Loeffler@users.noreply.github.com>
2024-02-17 20:49:45 +0100

    The gitconfig is now created by WiX/MSI during the install process. This way it is possible to add customizations in the GUI.

--- prepare.cmd
+++ prepare.cmd
@@ -1,5 +1,5 @@
-
-move PortableGit\etc\gitconfig PortableGit\etc\gitconfig.origin
-copy Prepared\etc\gitconfig PortableGit\etc\gitconfig
+del PortableGit\etc\gitconfig
+REM move PortableGit\etc\gitconfig PortableGit\etc\gitconfig.origin
+REM copy Prepared\etc\gitconfig PortableGit\etc\gitconfig
 
 heat dir PortableGit -var var.packageSource -scom -srd -sreg -sfrag -dr APPLICATIONROOTDIRECTORY -gg -ke -cg GIT_PORTABLE -t remove.xsl -o bundle.wxs 
